e402e7520295375978576ddd8793426360ad0ca5
Benz1905 <Chutikarn.D@users.noreply.github.com>
2020-02-10 14:47:06 +0700

    update

---
+++ app/src/main/java/th/ac/su/ict/chutikarn/bmiver2/ShowTextActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.su.ict.chutikarn.bmiver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowTextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_text)
+    }
+}
